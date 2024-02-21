ceed1b2d4c761ea1bc2b6a7e60e5fcb9538d1bf3
Ankit Chouhan <coderankit27@gmail.com>
2024-02-22 01:42:44 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
+import { error } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDB is Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
